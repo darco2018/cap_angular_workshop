871e9c43bfe33af54f1f30ee94eda39bae48a71c
dustrzyc <dariusz.ustrzycki@capgemini.com>
2024-12-18 15:13:51 +0100

    add basic validation to form

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { UserLoginType } from '../userLogin';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login-form',
   standalone: true,
   templateUrl: './login-form.component.html',
   styleUrl: './login-form.component.scss',
-  imports: [MatInputModule, MatButtonModule],
+  imports: [MatInputModule, MatButtonModule, FormsModule, CommonModule],
 })
 export class LoginFormComponent {
+  userLoginModel: UserLoginType = { password: '', username: '' };
 
+  onLogin() {
+    console.log('Login now...');
+  }
 }
