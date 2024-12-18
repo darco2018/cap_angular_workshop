1653b61bb2fd8e485757cd3bf0b40d91f896cd67
dustrzyc <dariusz.ustrzycki@capgemini.com>
2024-12-18 14:25:26 +0100

    add @material form

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-login-form',
-  imports: [],
+  standalone: true,
   templateUrl: './login-form.component.html',
-  styleUrl: './login-form.component.scss'
+  styleUrl: './login-form.component.scss',
+  imports: [MatInputModule, MatButtonModule],
 })
 export class LoginFormComponent {
 
