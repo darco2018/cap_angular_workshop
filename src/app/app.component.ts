0c48b58360bfdb3c29f1461cdb16003f0e351dc7
dustrzyc <dariusz.ustrzycki@capgemini.com>
2024-12-18 13:14:09 +0100

    create first componenet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'cost-evidence-app';
+  title = 'Cost Evidence';
+
+  logoClick() {
+    console.log('Logo has been clicked.');
+  }
 }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatToolbarModule],
+  template: `
+<mat-toolbar>
+  <!-- onLogoClick.emit() inserted dirrectly in html without creating a separate method -->
+  <span class="toolbar-logo" (click)="onLogoClick.emit()">{{header}}</span>
+</mat-toolbar>
+  `,
+  styleUrl: './header.component.scss'
+})
+
+
+export class HeaderComponent {
+
+  @Input({required: true}) public header = '';
+
+  @Output() onLogoClick = new EventEmitter<void>();
+
+}
