1fef229092d7e4c0999e1446ed13326645549871
dustrzyc <dariusz.ustrzycki@capgemini.com>
2024-12-19 11:40:57 +0100

    ad dashboard and login routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginContainerComponent } from './auth/login-container/login-container.component';
+import { DashboardContainerComponent } from './dashboard/dashboard-container/dashboard-container.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginContainerComponent },
+  { path: '', component: DashboardContainerComponent }
+];
---
+++ src/app/dashboard/dashboard-container/dashboard-container.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-container',
+  imports: [],
+  template: `
+    <p>
+      dashboard-container works!
+    </p>
+  `,
+  styleUrl: './dashboard-container.component.scss'
+})
+export class DashboardContainerComponent {
+
+}
