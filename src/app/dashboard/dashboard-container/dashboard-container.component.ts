0fcab5f598723270be4274970e9608c915344a67
dustrzyc <dariusz.ustrzycki@capgemini.com>
2024-12-21 11:35:13 +0100

    display categories in DashboardContainerComponent

--- src/app/dashboard/dashboard-container/dashboard-container.component.ts
+++ src/app/dashboard/dashboard-container/dashboard-container.component.ts
@@ -1,14 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DashboardService } from '../dashboard.service';
+import { DashboardCategory } from '../models/dashboard-category.type';
+import { CommonModule } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-container',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   template: `
-    <a [routerLink]="['/login']" >Back to login</a>
-  `,
-  styleUrl: './dashboard-container.component.scss'
-})
-export class DashboardContainerComponent {
+    <a [routerLink]="['/login']">Back to login</a>
 
+    
+    <div *ngFor="let category of categoryList">
+      {{ category | json }}
+    </div>
+  `,
+  styleUrl: './dashboard-container.component.scss',
+})
+export class DashboardContainerComponent implements OnDestroy {
+  private subscription?: Subscription;
+  categoryList: DashboardCategory[] = [];
+  constructor(private dashboardService: DashboardService) {
+    //this.ngOnInit();
+  }
+
+  // NECESSARY to avoid memory leaks: without it, when componenet is destroyed, 
+  //the subscription is not 
+  // But a better way to get data from Observables is to use async pipe
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
+
+  // when we load '', DASHBOARD_CATEGORIES_LIST is emitted
+  public ngOnInit(): void {
+    this.dashboardService.fetchDashboardCategories();
+    this.subscription = this.dashboardService.dashboardCategoryList$.subscribe((categories) => {
+      console.log(
+        'DashboardContainerComponent - ngOnInit - categories: ',
+        categories
+      );
+      this.categoryList = [...categories];
+    });
+  }
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { DashboardCategory } from './models/dashboard-category.type';
+import { DASHBOARD_CATEGORIES_LIST } from './constants/dashboard-categories';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DashboardService {
+  private _dashboardCategoryList$ = new BehaviorSubject<DashboardCategory[]>(
+    []
+  );
+  dashboardCategoryList$ = this._dashboardCategoryList$.asObservable();
 
-  constructor() { }
+  // simulates httpClient API call
+  public fetchDashboardCategories(): void {
+    // executes code after delay - simulates http response delay here
+    setTimeout(() => {
+      console.log(
+        'DashboardService - setTimeout - DASHBOARD_CATEGORIES_LIST:',
+        DASHBOARD_CATEGORIES_LIST
+      );
+      this._dashboardCategoryList$.next([...DASHBOARD_CATEGORIES_LIST]); //  emit a new value to all subscribers of this observable
+    }, 1000);
+    //This uses the spread operator (...) to create
+    //a shallow copy of the DASHBOARD_CATEGORIES_LIST array.
+    //By emitting a copy, you ensure that the original array remains unchanged
+    //even if the emitted array is modified by subscribers.
+  }
 }
