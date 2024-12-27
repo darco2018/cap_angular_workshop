d818e5050967cc95dee012e1ce97319ea84351cf
dustrzyc <dariusz.ustrzycki@capgemini.com>
2024-12-27 15:39:06 +0100

    add colors to icons

--- src/app/dashboard/constants/dashboard-categories.ts
+++ src/app/dashboard/constants/dashboard-categories.ts
@@ -1,10 +1,10 @@
 import { DashboardCategory } from "../models/dashboard-category.type";
 
 export const DASHBOARD_CATEGORIES_LIST: DashboardCategory[] = [
- {title: 'Home'},
- {title: 'Groceries'},
- {title: 'Sport'},
- {title: 'Investment'},
+ {title: 'Home', icon: 'piano', color: 'red'},
+ {title: 'Groceries', icon: 'face', color: 'blue'},
+ {title: 'Sport', icon: 'piano', color: 'green'},
+ {title: 'Investment', icon: 'piano', color: 'orange'},
  {title: 'Remodeling'},
  {title: 'Holiday'},
  {title: 'Pets'},
